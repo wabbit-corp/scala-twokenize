f37fb76057e0a974ec8fef0ad64ac5f07f883763
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2026-04-03 11:54:38 -0400

    chore(scala-twokenize): sync build config and workflows
    Align generated Gradle and GitHub workflow config.
    
    Semver Impact: NONE

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,16 +11,47 @@ plugins {
     scala
 
     id("maven-publish")
+
+    id("com.vanniktech.maven.publish")
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "one.wabbit"
-            artifactId = "scala-twokenize"
-            version = "0.0.2"
-            from(components["java"])
+mavenPublishing {
+    coordinates("one.wabbit", "scala-twokenize", "0.0.2")
+    publishToMavenCentral()
+    signAllPublications()
+    pom {
+        name.set("scala-twokenize")
+        description.set("scala-twokenize")
+        url.set("https://github.com/wabbit-corp/scala-twokenize")
+        licenses {
+            license {
+                name.set("GNU Affero General Public License v3.0 or later")
+                url.set("https://spdx.org/licenses/AGPL-3.0-or-later.html")
+            }
         }
+        developers {
+            developer {
+                id.set("wabbit-corp")
+                name.set("Wabbit Consulting Corporation")
+
+                email.set("wabbit@wabbit.one")
+
+            }
+        }
+        scm {
+            url.set("https://github.com/wabbit-corp/scala-twokenize")
+            connection.set("scm:git:git://github.com/wabbit-corp/scala-twokenize.git")
+            developerConnection.set("scm:git:ssh://git@github.com/wabbit-corp/scala-twokenize.git")
+        }
+    }
+}
+
+val localPublishRequested =
+    gradle.startParameter.taskNames.any { taskName -> "MavenLocal" in taskName }
+
+if (localPublishRequested) {
+    tasks.withType<org.gradle.plugins.signing.Sign>().configureEach {
+        enabled = false
     }
 }
 
@@ -36,6 +67,46 @@ dependencies {
 java {
     targetCompatibility = JavaVersion.toVersion(21)
     sourceCompatibility = JavaVersion.toVersion(21)
+}
+
+val configuredVersionString = version.toString()
+
+tasks.register("printVersion") {
+    inputs.property("configuredVersion", configuredVersionString)
+    doLast {
+        println(inputs.properties["configuredVersion"])
+    }
+}
+
+tasks.register("assertReleaseVersion") {
+    inputs.property("configuredVersion", configuredVersionString)
+    doLast {
+        val versionString = inputs.properties["configuredVersion"].toString()
+        require(!versionString.endsWith("+dev-SNAPSHOT")) {
+            "Release publishing requires a non-snapshot version, got $versionString"
+        }
+        val refType = System.getenv("GITHUB_REF_TYPE") ?: ""
+        val refName = System.getenv("GITHUB_REF_NAME") ?: ""
+        if (refType == "tag" && refName.isNotBlank()) {
+            val expectedTag = "v$versionString"
+            require(refName == expectedTag) {
+                "Git tag $refName does not match project version $versionString"
+            }
+        }
+    }
+}
+
+tasks.register("assertSnapshotVersion") {
+    inputs.property("configuredVersion", configuredVersionString)
+    doLast {
+        val versionString = inputs.properties["configuredVersion"].toString()
+        require(versionString.endsWith("+dev-SNAPSHOT")) {
+            "Snapshot publishing requires a +dev-SNAPSHOT version, got $versionString"
+        }
+        require((System.getenv("GITHUB_REF_TYPE") ?: "") != "tag") {
+            "Snapshot publishing must not run from a tag ref"
+        }
+    }
 }
 
 tasks {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,15 +20,21 @@ pluginManagement {
         id("org.jetbrains.kotlin.multiplatform") version "2.3.10"
         id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10"
         id("org.jetbrains.kotlin.plugin.compose") version "2.3.10"
-        id("com.android.application") version "8.13.2"
-        id("com.android.library") version "8.13.2"
-        id("com.android.kotlin.multiplatform.library") version "8.13.2"
+        id("com.android.application") version "8.12.0"
+        id("com.android.library") version "8.12.0"
+        id("com.android.kotlin.multiplatform.library") version "8.12.0"
         id("org.jetbrains.compose") version "1.9.1"
         id("com.gradleup.shadow") version "8.3.0"
         id("org.jetbrains.dokka") version "2.0.0"
         id("org.jetbrains.kotlinx.kover") version "0.9.3"
+        id("com.vanniktech.maven.publish") version "0.36.0"
         id("org.jetbrains.intellij") version "1.17.2"
         id("io.papermc.paperweight.userdev") version "1.7.2"
         id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
     }
+}
+
+val localSettingsFile = file("settings.local.gradle.kts")
+if (localSettingsFile.isFile) {
+    apply(from = localSettingsFile)
 }
